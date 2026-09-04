2a567830c9fdabe0ea1e8a38fe89ac401d0843e3
Juan <jhurtado1@student.scitech.edu>
2026-09-03 19:20:00 -0700

    Complete final exam project

---
+++ final-exam/js/script.js
@@ -0,0 +1,9 @@
+const registerButton = document.getElementById("register-btn");
+
+registerButton.addEventListener("click", function(event) {
+
+    event.preventDefault();
+
+    alert("Thank you for registering!");
+
+});
